0143f8e5c25ea8cf58906cc9c81a40ae74466ddd
RamiroLong <100241148+RamiroLong@users.noreply.github.com>
2022-09-15 20:03:59 -0300

    Adjunto mi PI, no es mucho pero es trabajo honesto

--- main.py
+++ main.py
@@ -2,35 +2,55 @@ from fastapi import FastAPI
 import pandas as pd
 import numpy as np
 import json
-app = FastAPI()
+
+#Para comenzar, importé fastAPI desde la terminal (mediante el comando pip install)
+#Desde GitBash, conecté la API con mi archivo main.py a través del servidor de uvicorn
+#Los comandos utilizados para conectar el servidor fueron: cd "(la ruta del archivo)" 
+#y uvicorn main:Zappi --reload
+#Luego de haber leído la documentación de fastAPI y familiarizado con sus diferentes comandos, utilicé el comando
+#get, para obtener en la Zappi lo que me devuelve la función
+
+Zappi = FastAPI()
+
+@Zappi.get("/")
+async def Bienvenida():
+    return "Bienvenido a mi primera Zappi y no de muzarella"
 
 
+#Item 1: Hallar el Año con más carreras
 
-#Item 1
-@app.get("/año_con_mas_carreras")
+@Zappi.get("/año_con_mas_carreras")
 async def mejor_año():
     races = pd.read_json(r'C:\Users\ramiro\Desktop\PI01_DATA03\Datasets\races.json')
     df_races = pd.DataFrame(races['year'].value_counts())
     df_races.reset_index(inplace = True) 
     return "El año con más carreras fue", int(df_races['index'][0])
 
-#Item 2
-@app.get("/Piloto_con_mas_puestos")
+
+#Item 2: Hallar el piloto con mayor cantidad de primeros puestos
+
+@Zappi.get("/Piloto_con_mas_puestos")
 async def mejor_piloto ():
     df_results = pd.read_json(r'C:\Users\ramiro\Desktop\PI01_DATA03\Datasets\results.json')
     primeros_puestos = df_results['position'] == 1
     df_results[primeros_puestos]['driverId'].value_counts()
     df_drivers = pd.read_json(r'C:\Users\ramiro\Desktop\PI01_DATA03\Datasets\drivers.json')
     return "El piloto con mas primeros puestos fue", str(df_drivers['name'][0]) 
-#Item 3
-@app.get("/Circuito_Mas_Corrido")
+
+
+#Item 3:Hallar el Nombre del circuito más corrido
+
+@Zappi.get("/Circuito_Mas_Corrido")
 async def circuito_corrido ():
     circuits = pd.read_json(r"C:\Users\ramiro\Desktop\PI01_DATA03\Datasets\circuits.json")
     races = pd.read_json(r'C:\Users\ramiro\Desktop\PI01_DATA03\Datasets\races.json')
     races['circuitId'].value_counts()
     return "El circuito más corrido fue", str(circuits['name'][13])
-#Item 4
-@app.get("/Piloto_con_mas_puntos_constructor_british_or_american")
+
+
+#Item 4: Hallar el Piloto con mayor cantidad de puntos en total, cuyo constructor sea de nacionalidad sea American o British
+
+@Zappi.get("/Piloto_con_mas_puntos_constructor_british_or_american")
 async def piloto_constructor_british_or_american ():
     df_constructors = pd.read_json(r"C:\Users\ramiro\Desktop\PI01_DATA03\Datasets\constructors.json", lines = True)
     df_results = pd.read_json(r'C:\Users\ramiro\Desktop\PI01_DATA03\Datasets\results.json')
